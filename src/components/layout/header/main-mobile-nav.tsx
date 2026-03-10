0889b926bf8e567b8b203c83d438bce6ee7dd2fa
byPronox <bypronoxyt@gmail.com>
2026-03-10 14:07:59 -0500

    update bug error fix

--- src/components/layout/header/main-mobile-nav.tsx
+++ src/components/layout/header/main-mobile-nav.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 import { navItems } from './nav-items';
 import { cn } from '@/lib/utils';
 import { useLocale } from '@/lib/locale';
